801f7e70dd38dc4f93676f15fc46d6339022454e
karasuex54 <karasuyama1997@gmail.com>
2020-11-21 16:53:08 +0900

    init

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <header>
+      <v-app-bar>
+        <v-toolbar-title>みんながんばってるよ</v-toolbar-title>
+      </v-app-bar>
+    </header>
+    <nuxt />
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-card class="mx-auto my-10"
+    max-width="400">
+      <v-card-title>
+        おはようございます
+      </v-card-title>
+      <v-card-text>
+        現在、{{count}} 人の方が起きています。
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  data: () => ({
+    count : 0,
+  }),
+  methods: {
+    async get_count_people(){
+      return 8;
+    }
+  },
+  async mounted() {
+    this.count = await this.get_count_people();
+  }
+}
+</script>
